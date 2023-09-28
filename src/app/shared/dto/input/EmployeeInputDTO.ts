edbe4e1672d2e58298f8269785ca48e94bec9b70
dalimiraoui <dalimiraoui48@gmail.com>
2023-09-28 12:31:34 +0100

    Display the Employee data list  in table , add pagination  & enable user to change the default items per page

---
+++ src/app/shared/dto/input/EmployeeInputDTO.ts
@@ -0,0 +1,14 @@
+export interface EmployeeInputDTO
+{
+    
+    imageUrl: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    contactNumber: string;
+    age: number;
+    dob: string;
+    salary: number;
+    address: string;
+
+}
